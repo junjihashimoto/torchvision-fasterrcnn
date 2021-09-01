97f3fa7c688dac42fcae7d599cc52eff8daa2eec
Junji Hashimoto <junji.hashimoto@gmail.com>
2021-09-01 22:49:34 +0900

    Move python scripts to src-directory

--- default.nix
+++ default.nix
@@ -21,7 +21,7 @@ let
       bdd100k
     ];
     buildInputs =  [];
-    src = ./.;
+    src = ./src;
     buildPhase = ''
       export CURL_CA_BUNDLE="/etc/ssl/certs/ca-certificates.crt"
       #export REQUESTS_CA_BUNDLE=""
@@ -56,16 +56,17 @@ let
       maintainers = with maintainers; [ junjihashimoto tscholak ];
     };
   };
-  mkDerivation2 = { pname, description, script, scriptArgs } : pkgs.stdenv.mkDerivation {
+  mkDerivation2 = { pname, description, script, scriptArgs, pretrained } : pkgs.stdenv.mkDerivation {
     pname = pname;
-    version = "1";
+    version = "2";
     nativeBuildInputs = [
       myPython
       pkgs.curl
       bdd100k
+      pretrained
     ];
     buildInputs =  [];
-    src = ./.;
+    src = ./src;
     buildPhase = ''
       export CURL_CA_BUNDLE="/etc/ssl/certs/ca-certificates.crt"
       #export REQUESTS_CA_BUNDLE=""
@@ -80,11 +81,12 @@ let
       ln -s ${bdd100k.out} bdd100k
       if [ ${script} = "train.py" ] ; then 
         python -m torch.distributed.launch --nproc_per_node=3 --use_env \
-          ${script} --output-dir "${scriptArgs.output}" --world-size 3
+          ${script} \
+            --resume "${pretrained.out}/output/checkpoint.pth" \
+            --epochs "${scriptArgs.epochs}" \
+            --output-dir "${scriptArgs.output}" --world-size 3
       else
         python ${script} \
-          --resume "${scriptArgs.resume}" 
-          --epochs "${scriptArgs.epochs}" 
           --output-dir "${scriptArgs.output}" 
       fi
     '';
@@ -102,8 +104,33 @@ let
       maintainers = with maintainers; [ junjihashimoto tscholak ];
     };
   };
+  checkpoint = pkgs.stdenv.mkDerivation {
+    pname = "checkpoint";
+    version = "1";
+    src = builtins.fetchurl {
+        "sha256"= "0jiir49zhc3m9w2d5d3wyzpl6lrrv294jqy4jllq322875avjx38";
+        "url"= "file:///home/hashimoto/git/torchvision-fastercnn-train/01/output/checkpoint.pth";
+    };
+    unpackCmd = ''
+      mkdir -p $out/output
+      cp "$curSrc" $out/output/"$'' + ''{curSrc#*-}"
+      sourceRoot=`pwd`
+    '';
+    dontFixup = true;
+    dontInstall = true;
+    meta = with lib; {
+      inherit description;
+      longDescription = ''
+      '';
+      homepage = "";
+      license = licenses.bsd3;
+      platforms = platforms.all;
+      maintainers = with maintainers; [ junjihashimoto tscholak ];
+    };
+  };
+  
 in
-{
+rec {
   train = mkDerivation {
     pname = "torchvision-fasterrcnn-trained";
     description = "Trained fasterrcnn";
@@ -115,10 +142,10 @@ in
   train2 = mkDerivation2 {
     pname = "torchvision-fasterrcnn-trained";
     description = "Trained fasterrcnn";
+    pretrained = checkpoint;
     script = "train.py";
     scriptArgs = {
       epochs = "56";
-      resume = "checkpoint.pth";
       output = "output";
     };
   };
